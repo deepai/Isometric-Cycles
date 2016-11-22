14a8f44393ebbbdd1b03d11799ec12ace7d82cc7
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-11-22 19:22:59 +0530

    Corrected Weights Entry in Edges of the Planar Graph

--- src/Creation_graph.cpp
+++ src/Creation_graph.cpp
@@ -12,6 +12,7 @@
 #include <boost/graph/boyer_myrvold_planar_test.hpp>
 
 #include "FileReader.h"
+#include "FileWriter.h"
 
 using namespace std;
 
@@ -24,9 +25,15 @@ typedef adjacency_list
   vecS,
   undirectedS,
   property<vertex_index_t, int>,
-  property<edge_index_t, int>
+  property<edge_index_t, int,
+    property<edge_weight_t,int> >
 > 
 Graph;
+
+typedef graph_traits<Graph>::edge_descriptor Edge;
+typedef property_map<Graph, edge_weight_t>::type Edge_Weight_Array;
+typedef property_map<Graph, edge_index_t>::type Edge_Index_Array;
+typedef graph_traits<Graph>::edge_iterator Edge_Iterator;
 
 int number_faces = 0;
 
@@ -42,40 +49,40 @@ typedef iterator_property_map<embedding_storage_t::iterator, property_map<Graph,
 
 struct output_visitor : public planar_face_traversal_visitor
 {
-  void begin_face()
-  {
-  	number_faces++;
-  }
-  void end_face()
-  {
-  	#ifdef PRINT_FACES
-  		printf("\n");		
-  	#endif
-  }
+        void begin_face()
+        {
+        	number_faces++;
+        }
+        void end_face()
+        {
+        	#ifdef PRINT_FACES
+        		printf("\n");		
+        	#endif
+        }
 };
 
 
 
 struct vertex_output_visitor : public output_visitor
 {
-  template <typename Vertex> 
-  void next_vertex(Vertex v) 
-  {
-  	#ifdef PRINT_FACES
-  		printf("%d ",v + 1);		
-  	#endif
-  }
+        template <typename Vertex> 
+        void next_vertex(Vertex v) 
+        {
+        	#ifdef PRINT_FACES
+        		printf("%d ",v + 1);		
+        	#endif
+        }
 };
 
 
 
 struct edge_output_visitor : public output_visitor
 {
-  template <typename Edge> 
-  void next_edge(Edge e) 
-  { 
+        template <typename Edge> 
+        void next_edge(Edge e) 
+        { 
 
-  }
+        }
 };
 
 
@@ -96,41 +103,48 @@ int main(int argc, char *argv[])
 	file_in.get_nodes_edges(num_nodes, num_edges);
 
 	Graph G(num_nodes);
+        Edge edge; 
+
+        Edge_Weight_Array edge_weights = get(edge_weight, G);
 
 	for(int i=0; i < num_edges; i++){
 		file_in.read_edge(src, dest, weight);
-		add_edge(src, dest, weight, G);
+		edge = add_edge(src, dest, G).first;
+                edge_weights[edge] = weight;
 	}
 
 
 	file_in.fileClose();
 
-	property_map<Graph, edge_index_t>::type e_index = get(edge_index, G);
-  	graph_traits<Graph>::edges_size_type edge_count = 0;
-  	graph_traits<Graph>::edge_iterator ei, ei_end;
-  	for(boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei)
-    	put(e_index, *ei, edge_count++);
+	Edge_Index_Array e_index = get(edge_index, G);
+        graph_traits<Graph>::edges_size_type edge_count = 0;
+        Edge_Iterator ei, ei_end;
+        for(boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei)
+                put(e_index, *ei, edge_count++);
 
-	embedding_storage_t embedding_storage(num_vertices(G));
-	embedding_t embedding(embedding_storage.begin(), get(vertex_index, G));
+        embedding_storage_t embedding_storage(num_vertices(G));
+        embedding_t embedding(embedding_storage.begin(), get(vertex_index, G));
 
-	bool is_planar_graph = false;
-	is_planar_graph = boyer_myrvold_planarity_test(boyer_myrvold_params::graph = G,
-                               							boyer_myrvold_params::embedding = embedding
-                               							);
+        bool is_planar_graph = false;
+        is_planar_graph = boyer_myrvold_planarity_test(boyer_myrvold_params::graph = G,
+                                                        boyer_myrvold_params::embedding = embedding
+                                       			);
 
-	if(is_planar_graph)
-	{
-		vertex_output_visitor v_vis;
-		edge_output_visitor e_vis;
-
-		planar_face_traversal(G, embedding, v_vis);
-
-		cout << "Number of faces = " << number_faces << endl;
-	}
-
-	cout << "Planarity test for graph yields " << ((is_planar_graph)?"true":"false") << endl;
+        // for (tie(ei, ei_end) = edges(G); ei != ei_end; ++ei)
+        //         cout << edge_weights[*ei] << endl;
 
 
-	return 0;
+        if(is_planar_graph)
+        {
+        	vertex_output_visitor v_vis;
+        	edge_output_visitor e_vis;
+
+        	planar_face_traversal(G, embedding, v_vis);
+
+        	cout << "Number of faces = " << number_faces << endl;
+        }
+
+        cout << "Planarity test for graph yields " << ((is_planar_graph)?"true":"false") << endl;
+        
+        return 0;
 }
