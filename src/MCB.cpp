8559465932874a77660ac9f33e5ad8cc4b65c2e3
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-12-10 11:48:37 +0530

    Reading and Mapping edges of plane and Dual graph.

--- src/MCB.cpp
+++ src/MCB.cpp
@@ -8,13 +8,9 @@
 #include <boost/ref.hpp>
 #include <vector>
 
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
-
-#include <boost/graph/planar_face_traversal.hpp>
-#include <boost/graph/boyer_myrvold_planar_test.hpp>
-#include <boost/graph/make_connected.hpp>
-#include <boost/graph/make_biconnected_planar.hpp>
 
 #include "FileReader.h"
 #include "FileWriter.h"
@@ -42,6 +38,79 @@ typedef graph_traits<Graph>::edge_iterator Edge_Iterator;
 
 int main(int argc, char *argv[])
 {
+	if(argc < 3)
+	{
+		printf("First Argument is the Input Graph\n");
+		printf("Second Argument is the Dual Graph\n");
+		return 0;
+	}
+	input_graph = string(argv[1]);
+	input_dual_graph = string(argv[2]);
+
+	int num_nodes_G, num_edges_G;
+	int num_nodes_dual_G, num_edges_dual_G;
+
+	int src, dest, weight;
+	FileReader file_in(input_graph.c_str());
+
+	file_in.get_nodes_edges(num_nodes_G, num_edges_G);
+
+	Graph G(num_nodes_G);
+    Edge edge;
+    Edge_Iterator ei, ei_end;
+
+    Edge_Weight_Array edge_weights = get(edge_weight, G);
+
+	for(int i=0; i < num_edges_G; i++){
+		file_in.read_edge(src, dest, weight);
+		edge = add_edge(src, dest, G).first;
+        edge_weights[edge] = weight;
+	}
+
+	file_in.fileClose();
+
+	FileReader file_dual_in(input_dual_graph.c_str());
+
+	file_dual_in.get_nodes_edges(num_nodes_dual_G, num_edges_dual_G);
+
+	Edge *edges_G = new Edge[num_edges_G]; //stores edges of G
+	Edge *edges_dual_G = new Edge[num_edges_G]; //stores edges of dual_G
+
+	int edge_id = 0, curr_edge = 0;
+
+	for(boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei)
+    {
+    	edges_G[edge_id++] = *ei; //stores edges of G in the array
+    }
+
+	Graph dual_G(num_nodes_dual_G);
+
+	Edge_Index_Array map_g_dual = get(edge_index, G); //maps edges from G to dual
+	Edge_Index_Array map_dual_g = get(edge_index, dual_G); //maps edges from dual to G
+
+	edge_weights = get(edge_weight, dual_G);
+
+	for(int i=0; i < num_edges_G; i++){
+		file_dual_in.read_edge(src, dest, weight);
+		edge = add_edge(src, dest, dual_G).first;
+        edge_weights[edge] = weight;
+	}
+
+	edge_id = 0;
+	curr_edge = 0;
+
+	FILE *fp = file_dual_in.get_file();
+
+	for(boost::tie(ei, ei_end) = edges(G); ei != ei_end; ++ei)
+    {
+    	edges_dual_G[curr_edge] = *ei;  //First store dual_G edges
+    	fscanf(fp,"%d", &edge_id);      //read the mapping of dual_to_G edges
+    	map_dual_g[*ei] = edge_id;		//map the dual to the prev read value
+    	map_g_dual[edges_G[edge_id]] = curr_edge;  //reverse map the edges[G] to curr_edge value
+    	curr_edge++;
+    }
+
+	file_dual_in.fileClose();
 
 	return 0;
 }
